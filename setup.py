31b75b051eed1181894ee2e95c3d4c61bbf3ceb2
IanSmith21 <43889567+IanSmith21@users.noreply.github.com>
2020-04-09 15:39:58 -0700

    Removed unneeded install_requires jupyter and pytest. (#3)
    * Removed unneeded install_requires jupyter and pytest.
    
    * Moved requirements into seperate file and split development and package requirements.

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
+import os.path
 from setuptools import setup, find_packages
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+requirements_fname = os.path.join(root_dir, 'requirements.txt')
+
+with open(requirements_fname) as fp:
+    install_requires = [i.strip() for i in fp.readlines()]
 
 setup(
     name="pyvisa-mock",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     python_requires='>=3.6.*',
-    install_requires=[
-        'pyvisa>=1.9.1',
-        'pytest',
-        'jupyter'
-    ]
+    install_requires=install_requires,
 )
