cf2a0ffa1c9887088ba160a433274166dce9737a
Liang Gao (KFORCE INC) <v-liagao@microsoft.com>
2020-04-22 14:39:56 -0700

    simplify the test_delay.py

--- visa_mock/test/base/test_delays.py
+++ visa_mock/test/base/test_delays.py
@@ -1,49 +1,35 @@
-from visa_mock.test.mock_instruments.instruments import Mocker1
+import pytest
 import time
+from visa_mock.base.base_mocker import BaseMocker
+from visa_mock.test.mock_instruments.instruments import Mocker1
+
+
+def time_command(mocker: BaseMocker, command: str) -> float:
+    start = time.time()
+    mocker.send(command)
+    return time.time() - start
 
 
 def test_delay_on_instrument():
-    call_delay = 1
+    call_delay = 1.0        # unit: [sec]
     mocker = Mocker1()
 
-    # At first, there is no delay:
-    t0 = time.time()
-    mocker.send(":INSTR:CHANNEL1:VOLT?")
-    call_time0 = time.time() - t0
-    assert 0 <= call_time0 < 0.1
-
-    # Now, to introduce the delay to the whole instrument
+    # By default, there is no delay:
+    time_w_no_delay = time_command(mocker, ":INSTR:CHANNEL1:VOLT?")
+    # To introduce the delay to the whole instrument:
     mocker.set_call_delay(call_delay)
-
-    # all the commands will have the same delay:
-    t1 = time.time()
-    mocker.send(":INSTR:CHANNEL1:VOLT 12")
-    call_time1 = time.time() - t1
-    assert 0 <= (call_time1 - call_time0 - call_delay) < 0.1
-
-    t2 = time.time()
-    voltage = mocker.send(":INSTR:CHANNEL1:VOLT?")
-    call_time2 = time.time() - t2
-    assert 0 <= (call_time2 - call_time0 - call_delay) < 0.1
-    assert voltage == "12.0"
+    time_w_delay = time_command(mocker, ":INSTR:CHANNEL1:VOLT?")
+    assert time_w_delay - time_w_no_delay == pytest.approx(call_delay, 0.1)
 
 
 def test_delay_on_command():
-    call_delay = 2
+    call_delay = 2.0        # unit: [sec]
     mocker = Mocker1()
-
-    cmd = ":INSTR:CHANNEL(.*):VOLT (.*)"
+    cmd_w_delay = ":INSTR:CHANNEL(.*):VOLT (.*)"
 
     # To introduce delay to one cmd only:
-    mocker.set_call_delay(call_delay, cmd)
-    assert mocker.__scpi_dict__[cmd].call_delay == call_delay
-    t0 = time.time()
-    mocker.send(":INSTR:CHANNEL1:VOLT 12")
-    t1 = time.time()
-    assert call_delay <= (t1 - t0) < call_delay + 0.1
-
-    # Other commands should not have any delay:
-    voltage = mocker.send(":INSTR:CHANNEL1:VOLT?")
-    t0 = time.time()
-    assert 0 <= (time.time() - t0) < 0.1
-    assert voltage == "12.0"
+    mocker.set_call_delay(call_delay, cmd_w_delay)
+    time_w_delay = time_command(mocker, ":INSTR:CHANNEL1:VOLT 12")
+    # Other comands should have no delay:
+    time_w_no_delay = time_command(mocker, ":INSTR:CHANNEL1:VOLT?")
+    assert time_w_delay - time_w_no_delay == pytest.approx(call_delay, 0.1)
