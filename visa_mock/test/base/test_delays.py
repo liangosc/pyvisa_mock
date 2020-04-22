64d5486446b65c5fa8a97a1298d93296967c260f
Liang Gao (KFORCE INC) <v-liagao@microsoft.com>
2020-04-22 13:58:30 -0700

    to assert the difference before and after

--- visa_mock/test/base/test_delays.py
+++ visa_mock/test/base/test_delays.py
@@ -9,19 +9,22 @@ def test_delay_on_instrument():
     # At first, there is no delay:
     t0 = time.time()
     mocker.send(":INSTR:CHANNEL1:VOLT?")
-    assert 0 <= (time.time() - t0) < 1
+    call_time0 = time.time() - t0
+    assert 0 <= call_time0 < 0.1
 
     # Now, to introduce the delay to the whole instrument
     mocker.set_call_delay(call_delay)
 
     # all the commands will have the same delay:
-    t0 = time.time()
+    t1 = time.time()
     mocker.send(":INSTR:CHANNEL1:VOLT 12")
-    assert call_delay <= (time.time() - t0) < call_delay + 1
+    call_time1 = time.time() - t1
+    assert 0 <= (call_time1 - call_time0 - call_delay) < 0.1
 
-    t0 = time.time()
+    t2 = time.time()
     voltage = mocker.send(":INSTR:CHANNEL1:VOLT?")
-    assert call_delay <= (time.time() - t0) < call_delay + 1
+    call_time2 = time.time() - t2
+    assert 0 <= (call_time2 - call_time0 - call_delay) < 0.1
     assert voltage == "12.0"
 
 
@@ -37,10 +40,10 @@ def test_delay_on_command():
     t0 = time.time()
     mocker.send(":INSTR:CHANNEL1:VOLT 12")
     t1 = time.time()
-    assert call_delay <= (t1 - t0) < call_delay + 1
+    assert call_delay <= (t1 - t0) < call_delay + 0.1
 
     # Other commands should not have any delay:
     voltage = mocker.send(":INSTR:CHANNEL1:VOLT?")
     t0 = time.time()
-    assert 0 <= (time.time() - t0) < 1
+    assert 0 <= (time.time() - t0) < 0.1
     assert voltage == "12.0"
